35732351f3e961603a3b0284e8438d1d11fbd91d
Xavier Collantes <collantes.xavier@gmail.com>
2023-08-27 20:53:08 -0700

    Remove all instances of Google auth
    Signed-off-by: Xavier Collantes <collantes.xavier@gmail.com>

--- passphrase/utils.py
+++ passphrase/utils.py
@@ -1,7 +1,6 @@
 """Utils for checking passphrase."""
 
 import streamlit as st
-from google.cloud import secretmanager
 
 
 def is_auth(main, url_args):
@@ -15,28 +14,13 @@ def is_auth(main, url_args):
        visual.
     """
     try:
-        if matches_passphrase(url_args["p"][0]):
+        if url_args["p"][0] in st.secrets.passphrases.p:
             return main()
         return access_denied()
     except KeyError:
         return access_denied()
 
 
-@st.cache_data(show_spinner=False)
-def matches_passphrase(candidate: str) -> bool:
-    """Check if passphrase is correct."""
-    with st.spinner("Verifying authentication ..."):
-        return get_passphrase() == candidate
-
-
 def access_denied():
     """Visual for access denied."""
     st.header("Access is denied")
-
-
-def get_passphrase() -> str:
-    """Get secret passphrase."""
-    secret_id = st.secrets.gcp_secrets.endpoint
-    client = secretmanager.SecretManagerServiceClient()
-    response = client.access_secret_version(request={"name": secret_id})
-    return response.payload.data.decode("UTF-8")
