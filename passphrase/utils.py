350543927ce7b581917edbf6f6fe693da2be6cf4
Xavier Collantes <collantes.xavier@gmail.com>
2023-10-04 00:11:07 -0700

    Add enter pass
    Signed-off-by: Xavier Collantes <collantes.xavier@gmail.com>

--- passphrase/utils.py
+++ passphrase/utils.py
@@ -3,7 +3,7 @@
 import streamlit as st
 
 
-def is_auth(main, url_args):
+def is_auth(main, url_args) -> None:
     """Checks for URL args to match passphrase.
 
     Args:
@@ -16,11 +16,21 @@ def is_auth(main, url_args):
     try:
         if url_args["p"][0] in st.secrets.passphrases.p:
             return main()
-        return access_denied()
+        return show_login()
     except KeyError:
-        return access_denied()
+        return show_login()
 
 
-def access_denied():
-    """Visual for access denied."""
-    st.header("Access is denied")
+def show_login() -> None:
+    """Render password box."""
+    st.header("Enter passphrase")
+    with st.form(key="passphrase_form"):
+        phrase_attempt: str = st.text_input(label="").strip()
+        submit = st.form_submit_button(label="Enter")
+
+    if submit:
+        if phrase_attempt in st.secrets.passphrases.p:
+            st.success("Login success. Please refresh page.")
+            st.experimental_set_query_params(p=phrase_attempt)
+        else:
+            st.error("Wrong passphrase")
